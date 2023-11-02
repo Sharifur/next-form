6eb274d60f353efa8f1a62c6baec2d405752172d
Sharifur Rahman <rsharifur824@gmail.com>
2023-11-02 12:32:39 +0600

    navbar design done

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 const Logo = () => {
     return ( 
-        <div>
-            Logo
-        </div>
+        <Link href="/"
+        className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer">
+            NextForm
+        </Link>
      );
 }
  
